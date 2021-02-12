55763f7fd6353da37502cc9f45e9feaa3b62cd23
Max Fechner <max.fechner@gmail.com>
2021-02-12 01:01:27 +0100

    - import adjustments

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
-import './date.js';
+import './raw.js';
 
 let d = new Date()
 console.log(d.hour, d.minute, d.second)
-console.log('week',d.weekNumber)
-console.log('format',d.format('DDD., DD.','en-US'))
+console.log('week', d.weekNumber)
+console.log(d.year, d.month, d.day)
+// console.log('format',d.format('DDD., DD.','en-US'))
--- raw.js
+++ raw.js
@@ -1,10 +1,15 @@
 
-import date from './date.mjs';
+import date from './raw.mjs';
 
 Object.defineProperties(Date.prototype, {
 	format: { value: function (f, l) { return date.format(this, f, l) } },
 	weekNumber: { get: function () { return date.getWeekNumber(this) } },
+	year: { get: function () { return String(this.getFullYear()) } },
+	month: { get: function () { return String(this.getMonth() + 1).padStart(2, '0') } },
+	day: { get: function () { return String(this.getDate()).padStart(2, '0') } },
 	hour: { get: function () { return String(this.getHours()).padStart(2, '0') } },
 	minute: { get: function () { return String(this.getMinutes()).padStart(2, '0') } },
 	second: { get: function () { return String(this.getSeconds()).padStart(2, '0') } },
+	date: { get: function () { return this.toISOString().slice(0, 10) } },
+	time: { get: function () { return this.toISOString().slice(11, 19) } },
 });
